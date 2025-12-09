ce958022c8eb2544f8efbe4e87d5730caea2d894
Lchesbr <lchesbr@gmail.com>
2025-12-08 20:25:25 -0500

    Left Boundary

--- Page-Knight/Assets/Scripts/MoveCamera.cs
+++ Page-Knight/Assets/Scripts/MoveCamera.cs
@@ -5,11 +5,13 @@ public class MoveCamera : MonoBehaviour
     public float moveSpeed = 5.0f;
     public float moveAmount = 2.5f;
     public bool canMove;
+    public PlayerController playerController;
     
     public float originalPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
         originalPos = GameObject.Find("Camera Mover").GetComponent<Rigidbody>().position.x;
         canMove = false;
         
@@ -18,18 +20,27 @@ public class MoveCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(canMove)
+
+        if (canMove == true)
         {
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-            
+
         }
 
-        if(transform.position.x > moveAmount + originalPos)
+        if (transform.position.x > moveAmount + originalPos)
         {
-            
+
             canMove = false;
             originalPos = transform.position.x;
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            canMove = true;
+            playerController.waitingForCamera = true;
 
+        }
     }
 }
--- Page-Knight/Assets/Scripts/PlayerController.cs
+++ Page-Knight/Assets/Scripts/PlayerController.cs
@@ -1,34 +1,72 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Rigidbody moveCameraRb;
+    private MoveCamera moveCamera;
+    private Transform cameraMover;
     public float speed = 5.0f;
     public float jumpPower = 10.0f;
+    public float pushback = 5.0f;
     public bool isOnPlatform = true;
     private float moveHorizontal;
     public float gravityMod;
-    
-
+    public float leftBound;
+    public float playerPos;
+    public bool waitingForCamera;
+    public float cameraWaitTime = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        cameraMover = GameObject.Find("Camera Mover").GetComponent<Transform>();
+        moveCameraRb = GameObject.Find("Camera Mover").GetComponent<Rigidbody>();
+        moveCamera = GameObject.Find("Camera Mover").GetComponent<MoveCamera>();
         Physics.gravity *= gravityMod;
-        
+        playerPos = transform.position.x;
+        waitingForCamera = false;
+        leftBound = cameraMover.position.x - 52.0f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        leftBound = cameraMover.position.x - 52.0f;
+
         if (Input.GetKeyDown(KeyCode.Space) && isOnPlatform)
         {
             isOnPlatform = false;
             playerRb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
         } 
 
+        if(waitingForCamera == true)
+        {
+            speed = 0;
+            StartCoroutine(WaitForCamera());
+
+        }
+
+        if (waitingForCamera == false)
+        {
+            speed = 10.0f;
+            
+        }
+
+        if (transform.position.x < leftBound)
+        {
+            transform.position = new Vector3( leftBound, transform.position.y, transform.position.z);
+            
+        }
+
+        
         moveHorizontal = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * moveHorizontal * speed * Time.deltaTime);
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -37,6 +75,19 @@ public class PlayerController : MonoBehaviour
         {
             isOnPlatform = true;
         }
-    }
 
+        if (collision.gameObject.CompareTag("Boundary"))
+        {
+            playerRb.AddForce(Vector3.right * pushback, ForceMode.Impulse);
+        }
+
+    }
+    IEnumerator WaitForCamera()
+    {
+        while(waitingForCamera == true)
+        {
+            yield return new WaitForSeconds(cameraWaitTime);
+            waitingForCamera = false;
+        }
+    }
 }
